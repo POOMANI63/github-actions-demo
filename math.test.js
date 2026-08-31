26333b816952eaa98a00c3f5c8a7cdb822657efd
POOMANI63 <poomani.m007@gmail.com>
2026-08-31 14:30:54 +0530

    Fix math unit test bug

--- math.test.js
+++ math.test.js
@@ -3,7 +3,7 @@ const { add } = require('./math');
 console.log("Running unit tests...");
 
 const result = add(2, 3);
-if (result !== 10) {
+if (result !== 5) {
   console.error(`Test Failed: Expected 5, but got ${result}`);
   process.exit(1);
 }
